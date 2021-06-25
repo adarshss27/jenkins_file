de4bc6c6fe39af0aa694bcfd42d4e105122390c5
adarshss27 <drsh_ss@yahoo.co.in>
2021-06-25 18:15:02 +0530

    new jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any
+    stages{
+        stage('git scm'){
+            steps{
+                git branch: 'main', url: 'https://github.com/adarshss27/jenkins_ansible_test.git'
+            }
+        }
+        stage('permission'){
+            steps{
+                sh 'chmod  -R 777 /var/lib/jenkins/workspace/test_pipe'
+            }
+        }
+        stage('copy'){
+            steps{
+                sh 'scp -r /var/lib/jenkins/workspace/test_pipe adarsh@192.168.235.145:/home/adarsh/new_project'
+            }
+        }
+        stage('deploy-1'){
+            steps{
+                sh 'ssh adarsh@192.168.235.145 "ansible-playbook /home/adarsh/new_project/ans_ble/gather_fact.yml"'                                                            
+            }
+        }
+        stage('deploy-2'){
+            steps{
+                sh 'ssh adarsh@192.168.235.145 "ansible-playbook /home/adarsh/new_project/ans_ble/gather_fact1.yml"'                                                            
+            }
+        }          
+    }
+
+}
